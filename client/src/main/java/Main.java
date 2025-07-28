37ce5ec74c3acd30c930f1309da406a50abc7fe5
cambriggs2112 <cambriggs2112@icloud.com>
2025-07-27 21:08:34 -0600

    Implemented register

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,26 +6,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("♕ Welcome to 240 Chess. Type Help to get started. ♕");
+        System.out.println();
+        String authToken = null;
         while (true) {
-            System.out.println();
-            System.out.print("[LOGGED_OUT] >>> ");
+            if (authToken == null) {
+                System.out.print("[LOGGED_OUT] >>> ");
+            } else {
+                System.out.print("[LOGGED_IN] >>> ");
+            }
             ArrayList<String> arguments = ClientMainFuncs.parseInput(input.nextLine());
             if (arguments.isEmpty()) {
                 continue;
             }
-            if (arguments.getFirst().equalsIgnoreCase("help")) {
-                System.out.println("\u001b[38;5;12m  register <USERNAME> <PASSWORD> <EMAIL>\u001b[39m - to create an account");
-                System.out.println("\u001b[38;5;12m  login <USERNAME> <PASSWORD>\u001b[39m - to play chess");
-                System.out.println("\u001b[38;5;12m  quit\u001b[39m - playing chess");
-                System.out.println("\u001b[38;5;12m  help\u001b[39m - with possible commands");
-            } else if (arguments.getFirst().equalsIgnoreCase("quit")) {
-                break;
-            } else if (arguments.getFirst().equalsIgnoreCase("login")) {
-                break;
-            } else if (arguments.getFirst().equalsIgnoreCase("register")) {
-                break;
+            if (authToken == null) {
+                if (arguments.getFirst().equalsIgnoreCase("help")) {
+                    System.out.println("\u001b[38;5;12m  register <USERNAME> <PASSWORD> <EMAIL>\u001b[39m - to create an account");
+                    System.out.println("\u001b[38;5;12m  login <USERNAME> <PASSWORD>\u001b[39m - to play chess");
+                    System.out.println("\u001b[38;5;12m  quit\u001b[39m - playing chess");
+                    System.out.println("\u001b[38;5;12m  help\u001b[39m - with possible commands");
+                    System.out.println();
+                } else if (arguments.getFirst().equalsIgnoreCase("quit")) {
+                    break;
+                } else if (arguments.getFirst().equalsIgnoreCase("login")) {
+                    break;
+                } else if (arguments.getFirst().equalsIgnoreCase("register")) {
+                    authToken = ClientMainFuncs.register(arguments);
+                } else {
+                    System.out.println("\u001b[38;5;160m  Unknown command. Type Help for a list of commands.\u001b[39m");
+                }
             } else {
-                System.out.println("\u001b[38;5;160m  Unknown command. Type Help for a list of commands.\u001b[39m");
+                if (arguments.getFirst().equalsIgnoreCase("help")) {
+                    System.out.println("\u001b[38;5;12m  register <USERNAME> <PASSWORD> <EMAIL>\u001b[39m - to create an account");
+                    System.out.println("\u001b[38;5;12m  login <USERNAME> <PASSWORD>\u001b[39m - to play chess");
+                    System.out.println("\u001b[38;5;12m  quit\u001b[39m - playing chess");
+                    System.out.println("\u001b[38;5;12m  help\u001b[39m - with possible commands");
+                    System.out.println();
+                } else if (arguments.getFirst().equalsIgnoreCase("quit")) {
+                    break;
+                } else {
+                    System.out.println("\u001b[38;5;160m  Unknown command. Type Help for a list of commands.\u001b[39m");
+                }
             }
         }
     }
--- client/src/main/java/ui/ClientMainFuncs.java
+++ client/src/main/java/ui/ClientMainFuncs.java
@@ -1,8 +1,11 @@
 package ui;
 
 import java.util.ArrayList;
+import model.*;
 
 public class ClientMainFuncs {
+    private static final String SERVER_URL = "http://localhost:8081";
+
     public static ArrayList<String> parseInput(String line) {
         ArrayList<String> result = new ArrayList<String>();
         for (int i = 0; i < line.length(); i++) {
@@ -20,5 +23,24 @@ public class ClientMainFuncs {
             }
         }
         return result;
+    }
+
+    public static String register(ArrayList<String> arguments) {
+        if (arguments.size() != 4) {
+            System.out.println("\u001b[38;5;160m  Usage: register <USERNAME> <PASSWORD> <EMAIL>\u001b[39m");
+            return null;
+        }
+        String username = arguments.get(1);
+        String password = arguments.get(2);
+        String email = arguments.get(3);
+        try {
+            ServerFacade sf = new ServerFacade(SERVER_URL);
+            RegisterResult res = sf.register(new RegisterRequest(username, password, email));
+            System.out.println("  Logged in as " + res.username());
+            return res.authToken();
+        } catch (ServiceException e) {
+            System.out.println("\u001b[38;5;160m  " + e + "\u001b[39m");
+            return null;
+        }
     }
 }
