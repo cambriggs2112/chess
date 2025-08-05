25e92b115a48818c89fb000ce056dc6d4bafd8f4
cambriggs2112 <cambriggs2112@icloud.com>
2025-08-05 13:13:49 -0600

    Created web socket on message functionality for server

--- server/src/main/java/server/WebSocketServer.java
+++ server/src/main/java/server/WebSocketServer.java
@@ -1,7 +1,12 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.eclipse.jetty.websocket.api.*;
+import websocket.commands.*;
+import websocket.messages.*;
+
 import java.io.*;
 
 @WebSocket
@@ -10,12 +15,33 @@ public class WebSocketServer {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
-
-        // 1. De-serialize message to user command
-        // 2. Check auth token and game ID
-        // 3. Save session to Connection Manager if not already in
-        // 4. Do something according to command type
-        // 5. Serialize and send output
-        session.getRemote().sendString("str");
+        Gson gson = new Gson();
+        UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
+        try {
+            SQLAuthDAO auths = new SQLAuthDAO();
+            SQLGameDAO games = new SQLGameDAO();
+            if (auths.getAuth(command.getAuthToken()) == null) {
+                session.getRemote().sendString(gson.toJson(new ErrorMessage("ERROR: Auth token is unknown.")));
+                return;
+            }
+            if (games.getGame(command.getGameID()) == null) {
+                session.getRemote().sendString(gson.toJson(new ErrorMessage("ERROR: Game ID is unknown.")));
+                return;
+            }
+            switch (command.getCommandType()) {
+                case CONNECT:
+                    ConnectionManager.addConnection(command.getAuthToken(), command.getGameID(), session);
+                    // send board, notify everyone else
+                case MAKE_MOVE:
+                    MakeMoveCommand moveCommand = gson.fromJson(message, MakeMoveCommand.class);
+                    // check move -> if valid, update and send board to all
+                case LEAVE:
+                    ConnectionManager.removeConnection(command.getAuthToken());
+                case RESIGN:
+                    // stop game and notify all
+            }
+        } catch (DataAccessException e) {
+            session.getRemote().sendString(gson.toJson(new ErrorMessage("ERROR: Unable to perform command:" + e.getMessage())));
+        }
     }
 }
