b6841996bef9741d4c936c813de8f4640492d654
cambriggs2112 <cambriggs2112@icloud.com>
2025-08-05 12:58:37 -0600

    Created connection manager

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -1,8 +1,10 @@
 package server;
 
 import java.util.HashMap;
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.*;
+import java.io.*;
 
 public class ConnectionManager {
     private static HashMap<String, Connection> connections = new HashMap<String, Connection>();
@@ -12,18 +14,31 @@ public class ConnectionManager {
     }
 
     public static void broadcastOne(String authToken, ServerMessage message) {
-
+        Gson gson = new Gson();
+        Connection conn = connections.get(authToken);
+        if (conn == null) {
+            return;
+        }
+        try (Session session = conn.session()) {
+            if (session.isOpen()) {
+                session.getRemote().sendString(gson.toJson(message));
+            } else {
+                removeConnection(authToken);
+            }
+        } catch (IOException e) {
+            removeConnection(authToken);
+        }
     }
 
     public static void broadcastAllExcept(String authToken, ServerMessage message) {
-
-    }
-
-    public static void broadcastAll(ServerMessage message) {
-
+        for (Connection conn : connections.values()) {
+            if (!conn.authToken().equals(authToken)) {
+                broadcastOne(authToken, message);
+            }
+        }
     }
 
     public static void removeConnection(String authToken) {
-
+        connections.remove(authToken);
     }
 }
